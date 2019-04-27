7abe32e462d85b40039c62ad6d2c8c7a52905218
Jahnvi Patel <jahnvi.patel30@gmail.com>
2019-04-27 16:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function shout(string) {
-  return string.toUpperCase()
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
 function logShout(string) {
-  console.log string.toUpperCase()
+  console.log(string.toUpperCase())
 }
